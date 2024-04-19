cfd092297d77767a38fc8fa2c9918d86a5c9dff7
helloworld <hello@world.com>
2024-04-19 18:40:51 +0800

    用php压缩markdown

--- src/App/Services/PlantUMLServices.php
+++ src/App/Services/PlantUMLServices.php
@@ -57,7 +57,7 @@ class PlantUMLServices
                 self::parseRelation($table, $relationStr);
             }
         }
-        return "$entityStr}$relationStr";
+        return "$entityStr}\n\n$relationStr";
     }
 
     /**

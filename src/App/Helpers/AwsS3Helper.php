9eba2e3f9b6f856d183db57a67c73b98a6dcc95e
helloworld <hello@world.com>
2024-04-25 11:40:54 +0800

    更新了aws s3的接口

--- src/App/Helpers/AwsS3Helper.php
+++ src/App/Helpers/AwsS3Helper.php
@@ -28,18 +28,23 @@ class AwsS3Helper
     ];
 
     /**
+     * 上传文件
      * @param string $fileName
      * @param string $fileContent
      * @param string|null $acl
      * @return void
+     * @throws Err
      */
     public static function PutObject(string $fileName, string $fileContent, ?string $acl = 'public'): void
     {
-        Storage::disk('s3')->put($fileName, $fileContent, $acl);
+        $result = Storage::disk('s3')->put($fileName, $fileContent, $acl);
+        if (!$result)
+            ee('文件上传失败');
     }
 
     /**
-     * @param string $fileName
+     * 获取文件
+     * @param string $fileName 文件名
      * @return string|null
      */
     public static function GetObject(string $fileName): ?string
@@ -48,20 +53,21 @@ class AwsS3Helper
     }
 
     /**
-     * @param string $fileName
-     * @param int|null $minutes
+     * 获取文件访问的 URL
+     * @param string $fileName 文件名
+     * @param int|null $minutes 有效时间
      * @return string
      */
     public static function TemporaryUrl(string $fileName, ?int $minutes = 30): string
     {
-        return Storage::disk('s3')
-            ->temporaryUrl($fileName, now()->addMinutes($minutes));
+        return Storage::disk('s3')->temporaryUrl($fileName, now()->addMinutes($minutes));
     }
 
     /**
-     * @param string $fileName
-     * @param int|null $minutes
-     * @param string|null $acl
+     * 获取临时上传的 URL
+     * @param string $fileName 文件名
+     * @param int|null $minutes 有效时间（分钟）
+     * @param string|null $acl 访问权限
      * @return array
      */
     public static function TemporaryUploadUrl(string $fileName, ?int $minutes = 30, ?string $acl = 'public-read'): array
@@ -83,19 +89,19 @@ class AwsS3Helper
 
     /**
      * 获取上传的参数
-     * @param string $dir
+     * @param string $uploadDir
      * @param string $fileName
      * @return array
      * @throws Err
      */
-    public static function PreUpload(string $dir, string $fileName): array
+    public static function PreUpload(string $uploadDir, string $fileName): array
     {
         $fileExt = last(explode('.', $fileName));
         if (!in_array($fileExt, self::ALLOW_FILE_EXTENSIONS))
             ee('文件类型不允许上传');
 
         $id = Ulid::generate();
-        return AwsS3Helper::temporaryUploadUrl("/upload/$dir/$id.$fileExt");
+        return AwsS3Helper::temporaryUploadUrl("/$uploadDir/$id.$fileExt");
     }
 
     /**
@@ -103,15 +109,34 @@ class AwsS3Helper
      * @param array $params
      * @param array $keys
      * @param string $uploadDir
+     * @param int|null $width
+     * @param int|null $quality
      * @return void
+     * @throws Err
      */
-    public static function ReplaceImageToOss(array &$params, array $keys, string $uploadDir): void
+    public static function ReplaceImageToOss(array &$params, array $keys, string $uploadDir, ?int $width = 1000, ?int $quality = 60): void
     {
         foreach ($keys as $key) {
             if (isset($params[$key])) {
-                $params[$key] = preg_replace_callback('/(<img[^>]+src=")([^">]+)(")/i', function ($matches) use ($uploadDir) {
+                $params[$key] = preg_replace_callback('/(<img[^>]+src=")([^">]+)(")/i', function ($matches) use ($uploadDir, $width, $quality) {
                     if ($matches[2] && strpos($matches[2], 'ata:image')) {
+                        // upload base64 image
                         $url = self::uploadBase64Image($matches[2], $uploadDir);
+                        logger()->channel('upload')->debug('Uploaded Base64 Image: ', [
+                            'url' => $url
+                        ]);
+
+                        // resize
+                        $key = self::getKeyByUrl($url);
+                        $content = file_get_contents($url . self::getOssParams($width, $quality));
+                        self::PutObject($key, $content);
+                        logger()->debug('Resized Base64 Image: ', [
+                            'url' => $url,
+                            'width' => $width,
+                            'quality' => $quality,
+                        ]);
+
+                        // return
                         return $matches[1] . $url . $matches[3];
                     }
                     return $matches[1] . $matches[2] . $matches[3];
@@ -121,9 +146,120 @@ class AwsS3Helper
     }
 
     /**
+     * 图片处理类
+     * @param array $images
+     * @param int|null $width
+     * @param int|null $quality
+     * @return void
+     * @throws Err
+     */
+    public static function CreateThumbUrl(array &$images, ?int $width = 64, ?int $quality = 50): void
+    {
+        $newImages = [];
+        foreach ($images as $image) {
+            if (!isset($image['thumbUrl'])) {
+                // 如果没有缩略图
+                $fileExt = last(explode('.', $image['url']));
+                $image['thumbUrl'] = str_replace(".$fileExt", "_thumb.$fileExt", $image['url']);
+                $content = file_get_contents($image['url'] . self::getOssParams($width, $quality));
+                AwsS3Helper::PutObject(self::getKeyByUrl($image['thumbUrl']), $content);
+                logger()->debug('Create Thumb Image: ', [
+                    'url' => $image['url'],
+                    'thumbUrl' => $image['thumbUrl'],
+                    'width' => $width,
+                    'quality' => $quality,
+                ]);
+            }
+            $newImages[] = $image;
+        }
+        $images = $newImages;
+    }
+
+    /**
+     * @param array $images
+     * @param int|null $width
+     * @param int|null $quality
+     * @return void
+     * @throws Err
+     */
+    public static function ResizeImages(array &$images, ?int $width = 64, ?int $quality = 50): void
+    {
+        $newImages = [];
+        foreach ($images as $image) {
+            // 如果有宽度，质量值，则跳过resize
+            if (!isset($image['width']) || !isset($image['quality'])){
+                $image['width'] = $width;
+                $image['quality'] = $quality;
+                $content = file_get_contents($image['url'] . self::getOssParams($width, $quality));
+                $key = self::getKeyByUrl($image['url']);
+
+                self::PutObject($key, $content);
+
+                logger()->debug('Resize Image: ', [
+                    'url' => $image['url'],
+                    'width' => $width,
+                    'quality' => $quality,
+                ]);
+            }
+            $newImages[] = $image;
+        }
+        $images = $newImages;
+    }
+
+    /**
+     * @param int|null $width
+     * @param int|null $quality
+     * @return string|void
+     * @throws Err
+     */
+    private static function getOssParams(?int $width, ?int $quality)
+    {
+        list(, $endpoint) = self::getBucketAndEndpoint();
+
+        if (str_contains($endpoint, 'myqcloud.com')) {
+            return "?imageMogr2/thumbnail/{$width}x/quality/$quality/minisize/1";
+        } elseif (str_contains($endpoint, 'aliyuncs.com')) {
+            return "?x-oss-process=image/resize,w_$width/quality,q_$quality";
+        } else {
+            ee('暂不支持的云存储');
+        }
+    }
+
+    /**
+     * @param string $url
+     * @return string
+     * @throws Err
+     */
+    private static function getKeyByUrl(string $url): string
+    {
+        list($bucket, $endpoint) = self::getBucketAndEndpoint();
+        return str_replace("https://$bucket.$endpoint", '', $url);
+    }
+
+    /**
+     * @return string[]
+     * @throws Err
+     */
+    private static function getBucketAndEndpoint(): array
+    {
+        $endpoint = config('filesystems.disks.s3.endpoint');
+        if (!$endpoint)
+            ee('请配置云存储的 endpoint');
+        $endpoint = str_replace('https://', '', $endpoint);
+
+        $bucket = config('filesystems.disks.s3.bucket');
+        if (!$bucket)
+            ee('请配置云存储的 bucket');
+
+        return [$bucket, $endpoint];
+    }
+
+    /**
+     * 上传 base64 图片
      * @param string $content
      * @param string $uploadDir
      * @return string
+     * @throws Err
      */
     private static function uploadBase64Image(string $content, string $uploadDir): string
     {
@@ -134,38 +270,9 @@ class AwsS3Helper
         $extension = substr($extension, 0, strpos($extension, ";"));
 
         $id = Ulid::generate();
-        $object = "/upload/$uploadDir/$id.$extension"; //sprintf("%s/%s", $uploadDir, uniqid(rand(1000, 9999)) . '.' . $extension);
+        $object = "/$uploadDir/$id.$extension";
 
-        logger()->debug("replaceImageToOss: $object");
         self::PutObject($object, $content);
         return Storage::disk('s3')->url($object);
-    }
-
-    /**
-     * 图片处理类，目前只适配了腾讯云
-     * @param array $images
-     * @param string $config
-     * @return void
-     */
-    public static function ProcessImages(array &$images, string $config): void
-    {
-        $newImages = [];
-        foreach ($images as $image) {
-            if (!isset($image['thumb_key'])) {
-                // 如果没有缩略图
-                $image['key'] = last(explode("myqcloud.com", $image['url']));
-                $image['thumb_key'] = str_replace('/upload/', '/upload/thumb_', $image['key']);
-                $image['thumb_config'] = $config;
-                $image['thumbUrl'] = str_replace('/upload/', '/upload/thumb_', $image['url']);
-                $content = file_get_contents($image['url'] . $config);
-                AwsS3Helper::PutObject($image['thumb_key'], $content);
-            } elseif (!isset($image['thumbUrl']) && isset($image['thumb_url'])) {
-                // 如果有缩略图，但没有缩略图 URL
-                $image['thumbUrl'] = $image['thumb_url'];
-                unset($image['thumb_url']);
-            }
-            $newImages[] = $image;
-        }
-        $images = $newImages;
     }
 }
